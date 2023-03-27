2b307842cbc0ce583d546d58c21ae5daf6a05b97
MasayaSonoda <machiya5454@gmail.com>
2023-03-27 16:25:21 +0900

    Update custom.js

--- feane-1.0.0/js/custom.js
+++ feane-1.0.0/js/custom.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(33.59554391844102, 130.4074161733906),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
