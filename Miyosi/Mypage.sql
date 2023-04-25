b10fa55e6d04e058e03bcc520cc4713aaca03717
miyosi33 <mhs.38@icloud.com>
2023-04-25 14:43:48 +0900

    Delete Mypage.sql

--- Miyosi/Mypage.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE member;
-USE member;
-CREATE TABLE user (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
