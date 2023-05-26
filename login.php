8cb4d7bce8f0fb05917187edd0dd94a0b49b9d77
MasayaSonoda <machiya5454@gmail.com>
2023-05-26 14:01:51 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php require 'header.php'; ?>
+
+
+<section class="book_section layout_padding">
+<div class="container1 tinntinn">
+    <div class="heading_container">
+    <h2>
+      Login
+    </h2>
+    </div>
+
+    
+    <div class="col-md-12">
+
+        <div class="form_container">
+            <div>
+            <input type="text" class="form-control" placeholder="Your Name" />
+            </div>
+            <div>
+            <input type="text" class="form-control" placeholder="Password" />
+            </div>
+            <a class="btn_box">
+            <button>
+                Login
+            </button>
+            </div>
+        </div>
+        <div class="heading_container chinchin">
+          <h2>
+            Register now
+          </h2>
+        </div>
+        <p align="left">※新規会員登録はこちらから</p>
+      
+          
+          <div class="col-md-12">
+          
+            <div class="form_container">
+              <div class="btn_box">
+                <a href="new.html">
+                  <button>
+                    Regist
+                  </button>
+                </a>
+              </div>
+            </div>
+
+          </div>
+</div>
+</section>
+
+
+<?php require 'footer.php'; ?>
