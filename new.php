6ec35d74c5777b69fdfa9711be569fd3a787e9ff
miyosi33 <mhs.38@icloud.com>
2023-05-26 14:07:13 +0900

    Create new.php

---
+++ new.php
@@ -0,0 +1,40 @@
+<?php // require 'server_connection.php'; ?>
+<?php require 'header.php'; ?>
+<!-- book section -->
+<section class="book_section layout_padding">
+<div class="container1 tinntinn">
+    <div class="heading_container">
+    <h2>
+        New
+    </h2>
+    </div>
+
+    
+    <div class="col-md-12">
+
+        <div class="form_container">
+        <form action="">
+            <div>
+            <input type="text" class="form-control" placeholder="Your Name" />
+            </div>
+            <div>
+            <input type="password" class="form-control" placeholder="Password" />
+            </div>
+            <div>
+            <input type="password" class="form-control" placeholder="Password" />
+            </div>
+            <div>
+              <input type="tel" class="form-control" placeholder="Phone Number" />
+            </div>
+            <a class="btn_box">
+              <button>
+                Register
+              </button>
+        </form>
+        </div>
+    
+    
+</section>
+<!-- end book section -->
+
+<?php require 'footer.php'; ?>
