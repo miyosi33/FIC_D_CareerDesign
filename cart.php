30812cd8475f733137df9ac41bdcf20fc07ff91c
MasayaSonoda <machiya5454@gmail.com>
2023-05-26 14:11:11 +0900

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,32 @@
+<?php require 'header.php'; ?>
+
+<section class="book_section layout_padding">
+<div class="container1 tinntinn">
+    <div class="heading_container">
+    <h2>
+      My cart
+    </h2>
+    </div>
+    
+    <div class="col-md-12">
+
+        <div class="form_container">
+        <form action="">
+            <div class="btn_box">
+            <button>
+                座席予約
+            </button>
+            </div>
+        </form>
+        </div>
+    
+    <div class="col-md-6">
+        </div>
+    </div>
+</div>
+</section>
+
+
+
+
+<?php require 'footer.php'; ?>
