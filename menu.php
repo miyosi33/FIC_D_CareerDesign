ef08a9ff32ebbfc9f458ca25a3041cf6d756591e
UesugiDaiki <koreha.apexyo@gmail.com>
2023-05-27 17:37:24 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -22,6 +22,63 @@
         <div class="row grid">
             <!-- 1 item -->
 
+          <?php
+            foreach($pdo->query('select * from 商品テーブル名') as $row){
+              $id = $row['id'];
+              echo '<div class="col-sm-6 col-lg-4 all ', 商品ジャンル, '">';
+              echo '<div class="box">';
+              echo '<div>';
+              echo '<div class="img-box">';
+              echo '<img src="', 商品画像ファイル, '">';
+              echo '</div>';
+              echo '<div class="detail-box">';
+              echo '<h5>', 商品名, '</h5>';
+              echo '<p>', 商品詳細, '</p>';
+              echo '<div class="quanity-div">';
+              echo '<form action="" class="quanity-form">';
+              echo '<label class="quanity-label">';
+              echo '<select name="quanity" class="quanity">';
+              echo '<option value="1">1</option>';
+              echo '<option value="2">2</option>';
+              echo '<option value="3">3</option>';
+              echo '<option value="4">4</option>';
+              echo '<option value="5">5</option>';
+              echo '<option value="6">6</option>';
+              echo '<option value="7">7</option>';
+              echo '<option value="8">8</option>';
+              echo '<option value="9">9</option>';
+              echo '<option value="10">10</option>';
+              echo '</select>';
+              echo '</form>';
+              echo '</div>';
+              echo '<div class="options">';
+              echo '<h6>';
+              echo 値段, '円';
+              echo '</h6>';
+              echo '<a href="">';
+              echo '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">';
+              echo '<g>';
+              echo '<g>';
+              echo '<path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />';
+              echo '</g>';
+              echo '</g>';
+              echo '<g>';
+              echo '<g>';
+              echo '<path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4C457.728,97.71,450.56,86.958,439.296,84.91z" />';
+              echo '</g>';
+              echo '</g>';
+              echo '<g>';
+              echo '<g>';
+              echo '<path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />';
+              echo '</g>';
+              echo '</g>';
+              echo '<g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>';
+              echo '</svg>';
+              echo '</a>';
+              echo '</div></div></div></div></div>';
+            }
+          ?>
+
           <div class="col-sm-6 col-lg-4 all food">
             <div class="box">
               <div>
