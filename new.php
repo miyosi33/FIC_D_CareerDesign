d5f3a9c3d505215b35085d0abc5a57e399ce2342
miyosi33 <mhs.38@icloud.com>
2023-05-28 10:27:41 +0900

    Update new.php

--- new.php
+++ new.php
@@ -12,7 +12,7 @@
     
     <div class="col-md-12">
 
-        <div class="form_container">
+        <d class="form_container">
         <form action="">
             <div>
             <input type="text" class="form-control" placeholder="Your Name" />
@@ -26,6 +26,7 @@
             <div>
               <input type="tel" class="form-control" placeholder="Phone Number" />
             </div>
+<<<<<<< HEAD
             </form>
             <div class="btn_box">
               <a href="login.php">
@@ -36,6 +37,23 @@
         
         </div>
     </div>
+=======
+        </form>
+    
+        <div class="col-md-12">
+          
+          <div class="form_container">
+            <div class="btn_box">
+              <a href="login.php">
+                <button>
+                  Regist
+                </button>
+              </a>
+            </div>
+          </div>
+
+        </div>
+>>>>>>> cc2cab5e838f82f91199b0b0f380207c50025cdc
     
 </section>
 <!-- end book section -->
