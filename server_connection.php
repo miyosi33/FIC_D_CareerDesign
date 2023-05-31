049ad36004eb2197c110b57c9cfc5532bb9fc6fb
UesugiDaiki <koreha.apexyo@gmail.com>
2023-05-31 14:21:54 +0900

    Update server_connection.php

--- server_connection.php
+++ server_connection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo =new PDO('mysql:host=localhost;dbname=kadai ;charset=utf8', 'staff', 'password');
+$pdo =new PDO('mysql:host=localhost;dbname=kadai;charset=utf8', 'staff', 'password');
 ?>
