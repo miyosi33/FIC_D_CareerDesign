00e41bb0d35a312ffbbffac2873ede345b19ed50
miyosi33 <mhs.38@icloud.com>
2023-06-05 09:26:29 +0900

    Update DB3.sql

--- DB3.sql
+++ DB3.sql
@@ -5,75 +5,75 @@ use kadai;
 
 /* 商品 */
 create table product (
-  product_id int auto_increment primary key, 
-  product_name varchar(100) not null, 
-  product_price int not null,
-  product_genre varchar(50) not null,
-  image_path varchar(200) not null,
-  product_description varchar(300) not null,
-  is_featured bit not null
+	product_id int auto_increment primary key, 
+	product_name varchar(100) not null, 
+	product_price int not null,
+	product_genre varchar(50) not null,
+	image_path varchar(200) not null,
+	product_description varchar(300) not null,
+	is_featured bit not null
 );
 
 /* 顧客 */
 create table customer (
-  id int auto_increment primary key, 
-  name varchar(100) not null, 
-  address varchar(100) not null, 
-  login varchar(100) not null unique, 
-  password varchar(100) not null
+	id int auto_increment primary key, 
+	name varchar(100) not null, 
+	address varchar(100) not null, 
+	login varchar(100) not null unique, 
+	password varchar(100) not null
 );
 
 /* 購入 */
 create table purchase (
-  id int auto_increment not null primary key, 
-  customer_id int not null, 
-  foreign key(customer_id) references customer(id)
+	id int auto_increment not null primary key, 
+	customer_id int not null, 
+	foreign key(customer_id) references customer(id)
 );
 
 /* 購入詳細 */
 create table purchase_detail (
-  purchase_id int not null, 
-  product_id int not null, 
-  count int not null, 
-  primary key(purchase_id, product_id), 
-  foreign key(purchase_id) references purchase(id), 
-  foreign key(product_id) references product(product_id)
+	purchase_id int not null, 
+	product_id int not null, 
+	count int not null, 
+	primary key(purchase_id, product_id), 
+	foreign key(purchase_id) references purchase(id), 
+	foreign key(product_id) references product(product_id)
 );
 
 /* 座席予約 */
 create table zasekiyoyaku (
-  reservation_id int auto_increment primary key,
-  reservation_datetime datetime not null,
-  seat_type int not null,
-  customer_id int not null,
-  foreign key(customer_id) references customer(id),
-  constraint check_reservation_datetime
-    check (
-      dayofweek(reservation_datetime) between 2 and 6
-      and (
-        (hour(reservation_datetime) >= 6 and hour(reservation_datetime) < 18)
-        or (dayofweek(reservation_datetime) in (1, 7) and hour(reservation_datetime) >= 7 and hour(reservation_datetime) < 19)
-      )
-      and minute(reservation_datetime) = 0
+	reservation_id int auto_increment primary key,
+	reservation_datetime datetime not null,
+	seat_type int not null,
+	customer_id int not null,
+	foreign key(customer_id) references customer(id),
+	constraint check_reservation_datetime
+		check (
+		dayofweek(reservation_datetime) between 2 and 6
+		and (
+			(hour(reservation_datetime) >= 6 and hour(reservation_datetime) < 18)
+			or (dayofweek(reservation_datetime) in (1, 7) and hour(reservation_datetime) >= 7 and hour(reservation_datetime) < 19)
+		)
+		and minute(reservation_datetime) = 0
     )
 );
 
 /* 商品予約 */
 create table syouhinyoyaku (
-  reservation_id int auto_increment primary key,
-  reservation_datetime datetime not null,
-  product_id int not null,
-  customer_id int not null,
-  foreign key(customer_id) references customer(id),
-  foreign key(product_id) references product(product_id),
-  constraint check_reservation_datetime
-    check (
-      dayofweek(reservation_datetime) between 2 and 6
-      and (
-        (hour(reservation_datetime) >= 6 and hour(reservation_datetime) < 18)
-        or (dayofweek(reservation_datetime) in (1, 7) and hour(reservation_datetime) >= 7 and hour(reservation_datetime) < 19)
-      )
-      and minute(reservation_datetime) = 0
+	reservation_id int auto_increment primary key,
+	reservation_datetime datetime not null,
+	product_id int not null,
+	customer_id int not null,
+	foreign key(customer_id) references customer(id),
+	foreign key(product_id) references product(product_id),
+	constraint check_reservation_datetime
+		check (
+		dayofweek(reservation_datetime) between 2 and 6
+		and (
+			(hour(reservation_datetime) >= 6 and hour(reservation_datetime) < 18)
+			or (dayofweek(reservation_datetime) in (1, 7) and hour(reservation_datetime) >= 7 and hour(reservation_datetime) < 19)
+		)
+		and minute(reservation_datetime) = 0
     )
 );
 
