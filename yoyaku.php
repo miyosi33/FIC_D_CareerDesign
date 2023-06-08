49eba96ab3e7a8fb6aa60afdfa97f77f97c07a24
TakumiNakahara <n.taku.150920@outlook.jp>
2023-06-08 13:54:47 +0900

    Update yoyaku.php

--- yoyaku.php
+++ yoyaku.php
@@ -9,29 +9,6 @@
       RESERVE
     </h2>
     </div>
-
-    
-    <div class="col-md-12">
-
-        <div class="form_container">
-        <form action="">
-            <div>
-            <input type="text" class="form-control" placeholder="Your Name" />
-            </div>
-            <div>
-            <input type="text" class="form-control" placeholder="Tel" />
-            </div>
-            <div>
-            <input type="text" class="form-control" placeholder="Email" />
-            </div>
-            <div class="btn_box">
-            <button>
-                RESERVE
-            </button>
-            </div>
-        </form>
-        </div>
-    
     <div class="col-md-6">
         <div class="map_container ">
         <div id="googleMap"></div>
@@ -39,6 +16,17 @@
     </div>
 </div>
 </section>
+
 <!-- end book section -->
 
 <?php require 'footer.php'; ?>
+
+<!-- お客さんの予約したものの情報を店舗側に送るようにする　ログインした状態のとき
+    必要なもの
+    　予約番号
+    　予約の日時
+    　予約する商品
+    　顧客情報
+    　商品削除
+
+    (お客さんが予約したものを店舗側で確認できるようにする) -->
