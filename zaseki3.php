21513393c754c419fef12552afe41544d7d74070
miyosi33 <mhs.38@icloud.com>
2023-06-13 14:02:33 +0900

    Update zaseki3.php

--- zaseki3.php
+++ zaseki3.php
@@ -27,4 +27,40 @@
     </div>
   </section>
   <!-- 他の必要なHTMLやJavaScriptのコードを追加 -->
+  <script>
+    const reservationDateInput = document.getElementById('reservation_date');
+    const reservationTimeStart = 6; // 平日の予約開始時間（時）
+    const reservationTimeEnd = 18; // 平日の予約終了時間（時）
+    const weekendReservationTimeStart = 7; // 土日祝の予約開始時間（時）
+    const weekendReservationTimeEnd = 19; // 土日祝の予約終了時間（時）
+    const reservationInterval = 15; // 予約時間帯の間隔（分）
+
+    reservationDateInput.addEventListener('input', function() {
+      const selectedDate = new Date(this.value);
+      const dayOfWeek = selectedDate.getDay();
+      const hour = selectedDate.getHours();
+      const minutes = selectedDate.getMinutes();
+
+      // 平日の予約時間帯制限
+      if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+        if (hour < reservationTimeStart || hour > reservationTimeEnd) {
+          this.setCustomValidity('予約は平日' + reservationTimeStart + ':00~' + reservationTimeEnd + ':00の間で行ってください');
+        } else if (minutes % reservationInterval !== 0) {
+          this.setCustomValidity('予約時間帯は' + reservationInterval + '分刻みで選択してください');
+        } else {
+          this.setCustomValidity('');
+        }
+      }
+      // 土日祝の予約時間帯制限
+      else if (dayOfWeek === 0 || dayOfWeek === 6) {
+        if (hour < weekendReservationTimeStart || hour > weekendReservationTimeEnd) {
+          this.setCustomValidity('予約は土日祝' + weekendReservationTimeStart + ':00~' + weekendReservationTimeEnd + ':00の間で行ってください');
+        } else if (minutes % reservationInterval !== 0) {
+          this.setCustomValidity('予約時間帯は' + reservationInterval + '分刻みで選択してください');
+        } else {
+          this.setCustomValidity('');
+        }
+      }
+    });
+  </script>
   <?php require 'footer.php'; ?>
