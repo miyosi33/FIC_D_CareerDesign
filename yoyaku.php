acb3bbbfa84855894717629df3d51a71eea468f7
TakumiNakahara <n.taku.150920@outlook.jp>
2023-06-14 13:41:56 +0900

    Update yoyaku.php

--- yoyaku.php
+++ yoyaku.php
@@ -2,75 +2,63 @@
 <?php require 'header.php'; ?>
 
 <!-- book section -->
-<section class="book_section layout_padding">
-<div class="container1 tinntinn">
-    <div class="heading_container">
-    <h2>
-      RESERVE
-    </h2>
+  <section class="book_section layout_padding">
+    <div class="container1 tinntinn">
+      <div class="heading_container">
+        <h2>RESERVE</h2>
+        <?php
+        ?>    
+      </div>
+      <div class="col-md-6">
+        <form action="" method="post">
+          <div class="form-group">
+            <label for="reservation_date">予約日時:</label>
+            <input type="datetime-local" id="reservation_date" name="reservation_date" required>
+          </div>
+
+        <buttton type="submit">予約する</button>
+        </form>
+      </div>
     </div>
-    <?php
-    // フォームが送信された場合の処理
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // 入力値を取得
-        $reservation_date = $_POST['reservation_date'];
-        $reservation_time = $_POST['reservation_time'];
-        $reservation_seat = $_POST['reservation_seat'];
+  </section>  
 
-        // 予約番号を生成する関数
-        function generateReservationNumber() {
-            $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            $reservation_number = '';
+  <script>
+    const reservationDateInput = document.getElementById('reservation_date');
+    const reservationTimeStart = 6; // 平日の予約開始時間（時）
+    const reservationTimeEnd = 18; // 平日の予約終了時間（時）
+    const weekendReservationTimeStart = 7; // 土日祝の予約開始時間（時）
+    const weekendReservationTimeEnd = 19; // 土日祝の予約終了時間（時）
+    const reservationInterval = 15; // 予約時間帯の間隔（分）
 
-            // 5文字のランダムな文字列を生成
-            for ($i = 0; $i < 5; $i++) {
-                $reservation_number .= $characters[rand(0, strlen($characters) - 1)];
-            }
+    reservationDateInput.addEventListener('input', function() {
+      const selectedDate = new Date(this.value);
+      const dayOfWeek = selectedDate.getDay();
+      const hour = selectedDate.getHours();
+      const minutes = selectedDate.getMinutes();
 
-            return $reservation_number;
+      // 平日の予約時間帯制限
+      if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+        if (hour < reservationTimeStart || hour > reservationTimeEnd) {
+          this.setCustomValidity('予約は平日' + reservationTimeStart + ':00~' + reservationTimeEnd + ':00の間で行ってください');
+        } else if (minutes % reservationInterval !== 0) {
+          this.setCustomValidity('予約時間帯は' + reservationInterval + '分刻みで選択してください');
+        } else {
+          this.setCustomValidity('');
         }
-
-        // 予約番号を生成
-        $reservation_number = generateReservationNumber();
-
-        // データベースに予約情報を追加する処理を実行することができます
-
-        // 以下は仮のメッセージの表示です
-        echo "商品予約が完了しました。";
-        echo "予約番号: " . $reservation_number;
-    }
-?>
-<div class="col-md-12">
-    <div class="form_container">
-    <form method="POST">
-        <!-- 予約フォームの入力項目を追加してください -->
-        <div class="form-group">
-            <label for="reservation_date">予約日</label>
-            <input type="date" name="reservation_date" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="reservation_time">予約時間</label>
-            <input type="time" name="reservation_time" class="form-control" required>
-        </div>
-        <div class="form-group">
-            
-        </div>
-        
-        <div class="btn_box">
-            <button type="submit">予約する</button>
-        </div>
-    </form>
-    </div>
-
-    <div class="col-md-6">
-        <div class="map_container ">
-        <div id="googleMap"></div>
-        </div>
-    </div>
-</div>
-</section>
-
-<?php require 'footer.php'; ?>
+      }
+      // 土日祝の予約時間帯制限
+      else if (dayOfWeek === 0 || dayOfWeek === 6) {
+        if (hour < weekendReservationTimeStart || hour > weekendReservationTimeEnd) {
+          this.setCustomValidity('予約は土日祝' + weekendReservationTimeStart + ':00~' + weekendReservationTimeEnd + ':00の間で行ってください');
+        } else if (minutes % reservationInterval !== 0) {
+          this.setCustomValidity('予約時間帯は' + reservationInterval + '分刻みで選択してください');
+        } else {
+          this.setCustomValidity('');
+        }
+      }
+    });
+  </script>
+  <?php require 'footer.php'; ?>
 
 <!-- お客さんの予約したものの情報を店舗側に送るようにする　ログインした状態のとき
     必要なもの
