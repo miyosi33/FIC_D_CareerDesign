2ddc2d0ffd6e9fbfae771b678fc3befb0adda2a1
UesugiDaiki <koreha.apexyo@gmail.com>
2023-06-19 13:51:14 +0900

    Create account.php

---
+++ account.php
@@ -0,0 +1,9 @@
+<?php require 'server_connection.php'; ?>
+<?php require 'header.php'; ?>
+
+<!-- アカウント情報 -->
+<?php
+echo '<p style="font-size: 100px">アカウント情報表示</p>';
+?>
+
+<?php require 'footer.php'; ?>
