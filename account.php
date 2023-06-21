5f0fabca1fa27fd801e185ba4c1301593fa54285
UesugiDaiki <koreha.apexyo@gmail.com>
2023-06-21 11:17:24 +0900

    Update account.php

--- account.php
+++ account.php
@@ -6,4 +6,9 @@
 echo '<p style="font-size: 100px">アカウント情報表示</p>';
 ?>
 
+<form action="login.php" method="post">
+    <input type="hidden" name="command" value="logout">
+    <input type="submit" value="ログアウト（仮）">
+</form>
+
 <?php require 'footer.php'; ?>
