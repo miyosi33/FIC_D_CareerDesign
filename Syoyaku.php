c8fa19245f2bed1128fa9cbd20e698ceb0338fc9
UesugiDaiki <koreha.apexyo@gmail.com>
2023-06-28 11:30:41 +0900

    Update Syoyaku.php

--- Syoyaku.php
+++ Syoyaku.php
@@ -1,20 +1,19 @@
 <?php require 'header.php'; ?>
 
-<?php
-// 予約が成功した場合の処理
-echo"<br>";
-echo"<br>";
-echo "<div class='V'><p>予約が完了しました!</p>";
-echo "<p>予約日時：" . $_REQUEST['reservation_date'] . "分です</p>";
-echo "予約した商品はマイページから確認してください。";
-echo $purchase_id;
-?>
-
-<section class="book_section">
-    <div class="button form_container">
-        <a href="index.php" class="btn_box">
-            <button>Homeに戻る</button>
-        </a>
+<section class="book_section layout_padding customer_section">
+    <div class="container">
+        <div class="heading_container V">
+            <h2>予約が完了しました！</h2>
+            <?php
+            echo '<p>予約日時：', $_REQUEST['reservation_date'], '</p>';
+            ?>
+            <p>予約履歴はアカウントページからご確認ください</p>
+            <div class="button form_container">
+                <a href="index.php" class="btn_box">
+                    <button>Home</button>
+                </a>
+            </div>
+        </div>
     </div>
 </section>
 
