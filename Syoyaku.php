2df02e311a3ec0263ba796c6d5b232338a614de1
UesugiDaiki <koreha.apexyo@gmail.com>
2023-06-28 10:49:23 +0900

    a

--- Syoyaku.php
+++ Syoyaku.php
@@ -1,24 +1,13 @@
 <?php require 'header.php'; ?>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // フォームから送信されたデータを取得
-    $reservationDate = $_POST['reservation_date'];
-    $product_id = $_POST['product_id'];
-
-    // データベースに座席予約情報を保存する処理を追加する
-    // ここにデータベースへのデータ挿入処理を追加します
-    $sql = $pdo->prepare("INSERT INTO seat_reservation (reservation_date, product_id, customer_id) VALUES (?, ?, ?)");
-
-    // 予約が成功した場合の処理
-    echo"<br>";
-    echo"<br>";
-    echo "<div class='V'><p>予約が完了しました!</p>";
-    echo "<p>予約日時：" . $reservationDate . "分です</p>";
-    echo "予約した商品はマイページから確認してください。";
-    echo $product_id;
-
-}
+// 予約が成功した場合の処理
+echo"<br>";
+echo"<br>";
+echo "<div class='V'><p>予約が完了しました!</p>";
+echo "<p>予約日時：" . $_REQUEST['reservation_date'] . "分です</p>";
+echo "予約した商品はマイページから確認してください。";
+echo $purchase_id;
 ?>
 
 <section class="book_section">
