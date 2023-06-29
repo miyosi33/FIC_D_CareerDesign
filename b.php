967f663b7faefa95dadd0665ad681d766e35b6d7
miyosi33 <mhs.38@icloud.com>
2023-06-29 13:52:02 +0900

    a

--- b.php
+++ b.php
@@ -1,8 +1,22 @@
 <?php
-
+$nextDay = date('Y-m-d', strtotime('+1 day'));
+$nextDayTime = date('H:i', strtotime('+1 day'));
 echo '<div class="form-group">';
                 echo '<label for="reservation_date">予約日時:</label>';
-                echo '<input type="datetime-local" id="reservation_date" name="reservation_date" required >';
+                echo '<input type="datetime-local" id="reservation_date" name="reservation_date" required $nextDay.'T'.$nextDayTime;>';
                 echo '</div>';
-
                 ?>
+
+<?php
+
+
+
+<?php
+$nextDay = date('Y-m-d', strtotime('+1 day'));
+$nextDayTime = date('H:i', strtotime('+1 day'));
+?>
+
+<?php echo '<div class="form-group">'; ?>
+  <?php echo '<label for="reservation_date">予約日時:</label>'; ?>
+  <?php echo '<input type="datetime-local" id="reservation_date" name="reservation_date" required min="' . $nextDay . 'T' . $nextDayTime . '">'; ?>
+<?php echo '</div>'; ?>
