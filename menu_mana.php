fe4a7ef179bd268b90da73835d2c71073ba86900
MasayaSonoda <machiya5454@gmail.com>
2023-06-29 10:51:01 +0900

    Update menu_mana.php

--- menu_mana.php
+++ menu_mana.php
@@ -7,7 +7,7 @@
     echo '<table class="table">';
     echo '<thead>';
     echo '<tr>';
-	echo '<th scope="col">商品番号</th><th scope="col">名前</th><th scope="col">値段</th><th scope="col">種類</th><th scope="col">説明</th>';
+	echo '<th scope="col">ID</th><th scope="col">名前</th><th scope="col">値段</th><th scope="col">種類</th><th scope="col">説明</th><th scope="col">お勧め</th>';
     echo '</tr>';
     echo '</thead>';
     foreach ($pdo->query('select * from product') as $row) {
@@ -18,6 +18,7 @@
         echo '<td>', $row['product_price'], '</td>';
         echo '<td>', $row['product_genre'], '</td>';
         echo '<td>', $row['product_description'], '</td>';
+        echo '<td>', $row['is_featured'], '</td>';
     	echo '</tr>';
     	echo "\n";
     }
