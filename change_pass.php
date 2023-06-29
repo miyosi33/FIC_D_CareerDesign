cc3fa1202c58e2f0672eeff6684a504e74e1b6e3
UesugiDaiki <koreha.apexyo@gmail.com>
2023-06-29 14:55:13 +0900

    a

--- change_pass.php
+++ change_pass.php
@@ -6,20 +6,25 @@
     <h2>
         Change password
     </h2>
+    <h6>
+      パスワード変更
+    </h6>
   </div>
     
   <div class="col-md-12">
     <div class="form_container">
       <form action="account.php" method="post">
         <input type="hidden" name="command" value="password">
-        <div>
-          <input type="password" class="form-control" placeholder="Password" name="password" required/>
-        </div>
-        <div>
-          <input type="password" class="form-control" placeholder="New password" name="new_password" required/>
-        </div>
-        <div>
-          <input type="password" class="form-control" placeholder="Confirm new password" name="confirm_new_password" required/>
+        <div class="font-unset">
+          <div>
+            <input type="password" class="form-control" placeholder="Password" name="password" required/>
+          </div>
+          <div>
+            <input type="password" class="form-control" placeholder="New password" name="new_password" required/>
+          </div>
+          <div>
+            <input type="password" class="form-control" placeholder="Confirm new password" name="confirm_new_password" required/>
+          </div>
         </div>
 
         <div class="form_container">
